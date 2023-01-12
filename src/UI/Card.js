49a6a9219e9c5e1c34f914933de07cb499953751
Karthik Nimmagadda <karthiknimmagadda@Karthiks-MacBook-Pro.local>
2023-01-12 12:36:18 -0800

    Update Card class to className

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,9 +1,8 @@
 import './Card.css';
 
 const Card = (props) => {
-    debugger;
     const classes= 'card ' + props.className;
-    return <div class={classes}>{props.children}</div>;
+    return <div className={classes}>{props.children}</div>;
 }
 
 export default Card;
